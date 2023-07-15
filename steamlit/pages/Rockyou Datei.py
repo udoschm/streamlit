16864bb35dd4809e4360d9e8e0852dad68a8b994
udoschm <102591157+udoschm@users.noreply.github.com>
2023-07-15 17:09:37 +0200

    f

--- steamlit/pages/Rockyou Datei.py
+++ steamlit/pages/Rockyou Datei.py
@@ -20,23 +20,47 @@ st.write("# Rockyou Datei👋")
 
 st.markdown(
     """
-    | vernachlässigbar  | x |
-    |-------------------|---|
-    | begrenzt          | x |
-    | beträchtlich      | x |
-    | existenzbedrohend | x |
+    Die Rockyou Datei wurde als Basis für die KI und die Hashcat-Regeln benutzt.\\
+    Die Datei alleine konnte bereits 4,85% der Passwörter knacken.\\
+    | Enthaltene Passwörter | 26 067 879         |
+    |-----------------------|--------------------|
+    | Gecrackte Passwörter  | 2 937 754 (4.85%)  |
     """
 )
 
 
+st.markdown(
+    """
+    Die folgenden Diagramme beziehen sich auf die Passwörter, welche durch die Liste geknackt worden sind.
+    """
+)
+
 image = "steamlit/images/crackedpw_train_cut_cracktime.jpg"
 st.image(image, caption='Cracktime')
+
+st.markdown(
+    """
+    Hallo! Ich bin Text!
+    """
+)
 
 image = "steamlit/images/crackedpw_train_cut_pw_complexity.jpg"
 st.image(image, caption='Passwortkomplexität')
 
+st.markdown(
+    """
+    Hallo! Ich bin Text!
+    """
+)
+
 image = "steamlit/images/crackedpw_train_cut_normalverteilung_passwortlänge.jpg"
 st.image(image, caption='Passwortlänge')
+
+st.markdown(
+    """
+    Hallo! Ich bin Text!
+    """
+)
 
 image = "steamlit/images/crackedpw_train_cut_percentage_distribution_of_letters.jpg"
 st.image(image, "Verteilung der Zeichen")
