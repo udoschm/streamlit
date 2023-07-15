e83eceb3b2e4ca7efb5dd343092060d16192bc6c
udoschm <102591157+udoschm@users.noreply.github.com>
2023-07-15 16:24:50 +0200

    f

--- steamlit/pages/KI-750 Iterationen.py
+++ steamlit/pages/KI-750 Iterationen.py
@@ -18,7 +18,7 @@ st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 st.write("# KI 750 Iterrationen 👋")
 
-image = "images/68_linkedin_found_without_hashes_utf-8_repair_cracktime.jpg"
+image = "https://github.com/udoschm/streamlit/blob/7c85a0b71ce4672a5ec34ba298d89e5da77a8993/steamlit/images/68_linkedin_found_without_hashes_utf-8_repair_cracktime.jpg"
 st.image(image, output_format="JPG")
 
 if st.button("Zurück"):
